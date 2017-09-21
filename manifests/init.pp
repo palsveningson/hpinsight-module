4ec5859925cc6416d5cd379fd41bd34c29ea0534
erobhal <robert.e.hallgren@ericsson.com>
2017-09-21 13:44:19 +0200

    Spec test changes and small re-organization
    Made some changes to make the spec test work.
    Made som minor re-organization of parameter definitions.
    Extended the manufacturer & virtual if block to include the services
    and snmp config (or we will get errors on non HP HW machines)

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,82 +5,68 @@
 class hpinsight(
   $snmp_manage             = true,
   $ensure                  = 'present',
-  $hpacucli                = $hpinsight_test::params::hpacucli,
-  $hpssacli                = $hpinsight_test::params::hpssacli,
-  $hp_snmp_agents          = $hpinsight_test::params::hp_snmp_agents,
-  $hp_health               = $hpinsight_test::params::hp_health,
-) inherits hpinsight_test::params {
+  $hpacucli                = $hpinsight::params::hpacucli,
+  $hpssacli                = $hpinsight::params::hpssacli,
+  $hp_snmp_agents          = $hpinsight::params::hp_snmp_agents,
+  $hp_health               = $hpinsight::params::hp_health,
+  $hpi_packages            = $hpinsight::params::pkgname,
+  $hp_snmp_package         = $hpinsight::params::hpsnmp_pkgname,
+) inherits hpinsight::params {
 
-# Load default parameters according to OS
-
-  case $::osfamily {
-    'RedHat', 'Suse': {
-      $hpi_packages    = $hpinsight_test::params::pkgname
-      $hp_snmp_package = $hpinsight_test::params::hpsnmp_pkgname
-        }
-      
-    'Debian': {
-      $hpi_packages = $hpinsight_test::params::pkgname
-    }
-
-    default: {
-      fail("supports osfamilies RedHat, Suse and Debian. Detected osfamily <${::osfamily}>.")
-    }
- } 
-
-# Validation of the snmp_manage input
-  validate_bool($snmp_manage)
+    # Validation of the snmp_manage input
+    validate_bool($snmp_manage)
  
 
-## We only start the installation if ensure is present, if it is not we will remove all packages (see below)
-  if $ensure == 'present' { 
+    ## We only start the installation if ensure is present, if it is not we will remove all packages (see below)
+    if $ensure == 'present' { 
   
-# Package Installation
-# For Virtual - No HP packages
-  if $::manufacturer == 'HP' and $::virtual == 'physical' {
-      package {[$hpi_packages, $hp_snmp_package]:
-      ensure  => 'present' }
+        # Package Installation
+        # For Virtual - No HP packages
+        if $::manufacturer == 'HP' and $::virtual == 'physical' {
+            package { $hpi_packages:
+            ensure  => 'present' }
 
-      # HP Storage - raid management tools, need to check HW model also
-      # hpssacli HP SSA is now standard, please check for requirement of hpacucli
-      # http://h20564.www2.hpe.com/hpsc/swd/public/detail?swItemId=MTX_b6a6acb9762443b182280db805#tab1
-      # Site specific requirement for hpacucli
-    if $::operatingsystemmajrelease < 7 {
-      package { [$hpacucli]:
-      ensure => 'present',
-     }
-    } else {
-      package { [$hpssacli]:
-      ensure => 'present',
-     }
+            if $hp_snmp_package {
+                package { $hp_snmp_package:
+                ensure  => 'present' }
+            }
+
+            # HP Storage - raid management tools, need to check HW model also
+            # hpssacli HP SSA is now standard, please check for requirement of hpacucli
+            # http://h20564.www2.hpe.com/hpsc/swd/public/detail?swItemId=MTX_b6a6acb9762443b182280db805#tab1
+            # Site specific requirement for hpacucli
+            if $::operatingsystemmajrelease < 7 {
+                package { [$hpacucli]:
+                    ensure => 'present', }
+            } else {
+                package { [$hpssacli]:
+                  ensure => 'present', }
+            }
+
+            service { [$hp_health]:
+                ensure  => running,
+                status  => 'hpasmxld || pgrep hpasmlited',
+                require => Package[$hpi_packages],
+            }
+
+
+            # SNMP  - Service configuration and hp-snmp config. Only if user wants to use the snmp-module to manage snmp.
+            # This will install hps snmp software aswell as net-snmp since it is a dependency.
+
+            if $snmp_manage == true {
+                class { '::snmp': } 
+
+                service { [$hp_snmp_agents]:
+                    ensure  => running,
+                    status  => 'pgrep cmahealthd',
+                    require => [ Package[$hpi_packages], Class[::snmp] ]    
+                }
+            }
+        }
     }
-  }
-
-    service { [$hp_health]:
-      ensure  => running,
-      status  => 'hpasmxld || pgrep hpasmlited',
-      require => Package[$hpi_packages],
-    }
-  }
-
-  
-
-
-# SNMP  - Service configuration and hp-snmp config. Only if user wants to use the snmp-module to manage snmp.
-# This will install hps snmp software aswell as net-snmp since it is a dependency.
-
-  if $snmp_manage == true {
-    class { '::snmp': } 
-
-    service { [$hp_snmp_agents]:
-      ensure  => running,
-      status  => 'pgrep cmahealthd',
-      require => [ Package[$hpi_packages], Class[::snmp] ]    
+    elsif $ensure == 'absent' {
+        package {[$hpi_packages, $hp_snmp_package, ]:
+        ensure  => 'absent' }
     }
 }
- elsif $ensure == 'absent' {
-      package {[$hpi_packages, $hp_snmp_package, ]:
-      ensure  => 'absent' }
-    }
 
-}
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,25 @@
 #Class with default values for common variables
 class hpinsight::params {
 #Names for packages  
-  $pkgname            = 'hp-health' #Default for Redhat, Suse and Debian
-  $hpsnmp_pkgname     = 'hp-snmp-agents' #Default for Redhat and Suse. Not available in Debian
+  $pkgname                 = 'hp-health' #Default for Redhat, Suse and Debian
 #Names for services - Default for all supported OS
   $hp_snmp_agents          = 'hp-snmp-agents'
   $hp_health               = 'hp-health'
 # Below are HP Storage - raid management tool names, default for all OS
   $hpacucli                = 'hpacucli'
   $hpssacli                = 'hpssacli'
+
+  case $::osfamily {
+    'RedHat', 'Suse': {
+      $hpsnmp_pkgname = 'hp-snmp-agents'
+        }
+      
+    'Debian': {
+      $hpsnmp_pkgname = undef
+    }
+
+    default: {
+      fail("supports osfamilies RedHat, Suse and Debian. Detected osfamily <${::osfamily}>.")
+    }
+ } 
 }
